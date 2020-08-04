88896008efc7299cc1f50c4ae3ce99501e94b92b
Tobey <bitsqiu@gmail.com>
2020-08-04 20:27:50 +0800

    [qd] fixed: RsController.getRsEventBetween

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -2,9 +2,9 @@ package com.thoughtworks.rslist.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -24,5 +24,20 @@ public class RsController {
     @GetMapping("/rs/{index}")
     public RsEvent getOneRsEvent(@PathVariable int index) {
         return rsList.get(index - 1);
+    }
+
+    @GetMapping("/rs")
+    public List<RsEvent> getRsEventBetween(@RequestParam(required = false) Integer start,
+                                           @RequestParam(required = false) Integer end) {
+        if (start == null && end == null) {
+            return rsList;
+        }
+        if (end == null) {
+            return rsList.subList(start - 1, rsList.size());
+        }
+        if (start == null) {
+            return rsList.subList(0, end);
+        }
+        return rsList.subList(start - 1, end);
     }
 }
