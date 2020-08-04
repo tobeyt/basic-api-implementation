ee00e06226221c3b168e85fed4bd325c8dfb40ed
Tobey <bitsqiu@gmail.com>
2020-08-04 20:18:09 +0800

    [qd] fixed: RsControllerTest.shouldGetRsEventListBetween, RsControllerTest.shouldGetRsEventListBetweenHaveStartNotHaveEnd,  RsControllerTest.shouldGetRsEventListBetweenNotHaveStartHaveEnd and RsControllerTest.shouldGetRsEventListBetweenNotHaveStartNotHaveEnd.

--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -36,4 +36,47 @@ public class RsControllerTest {
                 .andExpect(jsonPath("$.keyWord", is("second")))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void shouldGetRsEventListBetween() throws Exception {
+        mockMvc.perform(get("/rs?start=2&end=3"))
+                .andExpect(jsonPath("$[1].eventName", is("the second event")))
+                .andExpect(jsonPath("$[1].keyWord", is("second")))
+                .andExpect(jsonPath("$[2].eventName", is("the third event")))
+                .andExpect(jsonPath("$[2].keyWord", is("third")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldGetRsEventListBetweenHaveStartNotHaveEnd() throws Exception {
+        mockMvc.perform(get("/rs?start=2"))
+                .andExpect(jsonPath("$[1].eventName", is("the second event")))
+                .andExpect(jsonPath("$[1].keyWord", is("second")))
+                .andExpect(jsonPath("$[2].eventName", is("the third event")))
+                .andExpect(jsonPath("$[2].keyWord", is("third")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldGetRsEventListBetweenNotHaveStartHaveEnd() throws Exception {
+        mockMvc.perform(get("/rs?end=2"))
+                .andExpect(jsonPath("$[1].eventName", is("the second event")))
+                .andExpect(jsonPath("$[1].keyWord", is("second")))
+                .andExpect(jsonPath("$[2].eventName", is("the third event")))
+                .andExpect(jsonPath("$[2].keyWord", is("third")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldGetRsEventListBetweenNotHaveStartNotHaveEnd() throws Exception {
+        mockMvc.perform(get("/rs"))
+                .andExpect(jsonPath("$[0].eventName", is("the first event")))
+                .andExpect(jsonPath("$[0].keyWord", is("first")))
+                .andExpect(jsonPath("$[1].eventName", is("the second event")))
+                .andExpect(jsonPath("$[1].keyWord", is("second")))
+                .andExpect(jsonPath("$[2].eventName", is("the third event")))
+                .andExpect(jsonPath("$[2].keyWord", is("third")))
+                .andExpect(status().isOk());
+    }
+
 }
