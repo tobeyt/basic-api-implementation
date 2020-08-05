3b1ec29e3d2c2e029588b9ab95b108411725588a
Tobey <bitsqiu@gmail.com>
2020-08-05 12:30:41 +0800

    [QD] fixed: UserControllerTest.shouldRegisterUser

--- src/main/java/com/thoughtworks/rslist/domain/User.java
+++ src/main/java/com/thoughtworks/rslist/domain/User.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.rslist.domain;
 
+import lombok.Data;
+
+@Data
 public class User {
+    private String userName;
+    private Integer age;
+    private String gender;
+    private String email;
+    private String phone;
+    private int vote = 10;
 }
--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -1,7 +1,32 @@
 package com.thoughtworks.rslist.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
 
+    @Test
+    void shouldRegisterUser() throws Exception {
+        User user = new User();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userJson = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(userJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 }
