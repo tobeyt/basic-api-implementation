cf80e48044c8b64b65fbc787b16a97ae56cd6b61
Tobey <bitsqiu@gmail.com>
2020-08-06 13:58:03 +0800

    [QD] fixed: RsListApplicationTests.shouldAddUser

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -1,7 +1,10 @@
 package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.domain.User;
+import com.thoughtworks.rslist.entity.UserEntity;
 import com.thoughtworks.rslist.exception.CommentError;
+import com.thoughtworks.rslist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,12 +16,22 @@ import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
 
     public static List<User> users = new ArrayList<>();
 
     @PostMapping("/user")
     public ResponseEntity register(@RequestBody @Valid User user) {
-        users.add(user);
+        UserEntity userEntity = UserEntity.builder()
+                .userName(user.getUserName())
+                .gender(user.getGender())
+                .age(user.getAge())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .vote(user.getVote())
+                .build();
+        userRepository.save(userEntity);
         return new ResponseEntity(null, HttpStatus.CREATED);
     }
 
@@ -28,7 +41,7 @@ public class UserController {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex){
+    public ResponseEntity MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
         CommentError commentError = new CommentError();
         commentError.setError("invalid user");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(commentError);
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,26 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class RsListApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    MockMvc mockMvc;
 
+    @Test
+    void shouldAddUser() throws Exception {
+        String userJson = "{\"userName\":\"qindi\",\"age\":22,\"gender\":\"male\",\"email\":\"bitsqiu@gmail.com\",\"phone\":\"13886585124\"}";
+        mockMvc.perform(post("/user").content(userJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
 }
